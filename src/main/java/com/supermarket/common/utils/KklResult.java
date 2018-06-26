1ca84d1c16b88b1d38f71eaff0afc1c0f5763a6f
KKLA1 <31823765+KKLA1@users.noreply.github.com>
2018-06-26 23:17:44 +0800

    file upload

---
+++ src/main/java/com/supermarket/common/utils/KklResult.java
@@ -0,0 +1,156 @@
+package com.supermarket.common.utils;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * supermarket-kkl response object
+ */
+public class KklResult implements Serializable{
+
+    /**
+	 * network transmit
+	 */
+	private static final long serialVersionUID = -5927924481595404184L;
+
+	// 定义jackson对象
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    // 响应业务状态
+    private Integer status;
+
+    // 响应消息
+    private String msg;
+
+    // 响应中的数据
+    private Object data;
+
+    public static KklResult build(Integer status, String msg, Object data) {
+        return new KklResult(status, msg, data);
+    }
+
+    public static KklResult ok(Object data) {
+        return new KklResult(data);
+    }
+
+    public static KklResult ok() {
+        return new KklResult(null);
+    }
+
+    public KklResult() {
+
+    }
+
+    public static KklResult build(Integer status, String msg) {
+        return new KklResult(status, msg, null);
+    }
+
+    public KklResult(Integer status, String msg, Object data) {
+        this.status = status;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public KklResult(Object data) {
+        this.status = 200;
+        this.msg = "OK";
+        this.data = data;
+    }
+
+//    public Boolean isOK() {
+//        return this.status == 200;
+//    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    /**
+     * 将json结果集转化为KklResult对象
+     * 
+     * @param jsonData json数据
+     * @param clazz KklResult中的object类型
+     * @return
+     */
+    public static KklResult formatToPojo(String jsonData, Class<?> clazz) {
+        try {
+            if (clazz == null) {
+                return MAPPER.readValue(jsonData, KklResult.class);
+            }
+            JsonNode jsonNode = MAPPER.readTree(jsonData);
+            JsonNode data = jsonNode.get("data");
+            Object obj = null;
+            if (clazz != null) {
+                if (data.isObject()) {
+                    obj = MAPPER.readValue(data.traverse(), clazz);
+                } else if (data.isTextual()) {
+                    obj = MAPPER.readValue(data.asText(), clazz);
+                }
+            }
+            return build(jsonNode.get("status").intValue(), jsonNode.get("msg").asText(), obj);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    /**
+     * 没有object对象的转化
+     * 
+     * @param json
+     * @return
+     */
+    public static KklResult format(String json) {
+        try {
+            return MAPPER.readValue(json, KklResult.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Object是集合转化
+     * 
+     * @param jsonData json数据
+     * @param clazz 集合中的类型
+     * @return
+     */
+    public static KklResult formatToList(String jsonData, Class<?> clazz) {
+        try {
+            JsonNode jsonNode = MAPPER.readTree(jsonData);
+            JsonNode data = jsonNode.get("data");
+            Object obj = null;
+            if (data.isArray() && data.size() > 0) {
+                obj = MAPPER.readValue(data.traverse(),
+                        MAPPER.getTypeFactory().constructCollectionType(List.class, clazz));
+            }
+            return build(jsonNode.get("status").intValue(), jsonNode.get("msg").asText(), obj);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+}
